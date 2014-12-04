813b2dc3417432c970389aca8e6e01c571eb71a8
Thibaut Specker <specker.thibaut@gmail.com>
2014-12-05 00:51:34 +0100

    model menu

---
+++ NuitInfoMobile/app/src/main/java/com/nuitinfo/model/Menu.java
@@ -0,0 +1,19 @@
+package com.nuitinfo.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by tspecker on 05/12/14.
+ */
+public class Menu {
+
+    public ArrayList<CustomMenuItem> items;
+
+    public class CustomMenuItem {
+
+        public String label;
+
+        public int image;
+
+    }
+}
