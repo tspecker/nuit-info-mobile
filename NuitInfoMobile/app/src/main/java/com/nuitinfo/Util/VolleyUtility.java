9eed4f96464486eab3d1f4576d75d46f89ab5347
tiby <fordevelopmenttiby@gmail.com>
2014-12-04 22:49:07 +0100

    3rd commit
    commit third

---
+++ NuitInfoMobile/app/src/main/java/com/nuitinfo/Util/VolleyUtility.java
@@ -0,0 +1,9 @@
+package com.nuitinfo.Util;
+
+/**
+ * Created by tiby on 04/12/2014.
+ */
+public class VolleyUtility {
+    
+
+}
---
+++ NuitInfoMobile/app/src/main/java/com/nuitinfo/model/Users.java
@@ -0,0 +1,35 @@
+package com.nuitinfo.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by tiby on 04/12/2014.
+ */
+public class Users {
+
+    public ArrayList<User> users;
+
+    public Users(){
+
+    }
+
+    public class User{
+
+        public int id ;
+
+        public User(){
+
+        }
+
+        public Error Login(String pseudo , String password){
+            Error result = null ;
+
+            /*
+             TODO :Coder la fonction
+              */
+
+            return result;
+        }
+
+    }
+}
