0cfcb0e970c73300af112401aed84e43b6765f70
tiby <fordevelopmenttiby@gmail.com>
2014-12-05 00:14:32 +0100

    4th commit
    4th commit

--- NuitInfoMobile/app/src/main/java/com/nuitinfo/Util/VolleyUtility.java
+++ NuitInfoMobile/app/src/main/java/com/nuitinfo/Util/VolleyUtility.java
@@ -1,9 +1,70 @@
 package com.nuitinfo.Util;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+import com.nuitinfo.model.ParamRequests;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 /**
  * Created by tiby on 04/12/2014.
  */
 public class VolleyUtility {
-    
 
+    public Error Post_Request(String url ,final ParamRequests paramObj) {
+        Error result = null;
+        String language = Locale.getDefault().getLanguage();
+        String tag_json_obj = "json_obj_req";
+        final String TAG = "Volley";
+        StringRequest jsonObjReq = new StringRequest(Method.POST,
+                url,
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                NetworkResponse responseNetwork = error.networkResponse;
+                if (responseNetwork != null) {
+
+
+                }
+
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+
+                for(ParamRequests.ParamRequest param : paramObj.list){
+                    params.put(param.Key , param.Value);
+                }
+                return params;
+            }
+
+        };
+
+        // Adding request to request queue
+        int socketTimeout = 50000;//30 seconds - change to what you want
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, 1, 1);
+        jsonObjReq.setRetryPolicy(policy);
+        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
+
+        return result;
+    }
 }
